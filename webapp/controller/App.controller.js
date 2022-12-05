8d43d8977b49b28a26706586955a04d6184bc478
sapbtp cloud <cloudsapbtp@gmail.com>
2022-12-05 07:09:40 +0000

    first com

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("fioricicddemo2.controller.App", {
+        onInit() {
+        }
+      });
+    }
+  );
+  
